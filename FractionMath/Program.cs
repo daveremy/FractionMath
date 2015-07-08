e169b760009d29f1b5f1f92714ccfb79d2e3102d
Dave Remy <email@daveremy.com>
2015-07-08 16:36:05 -0400

    Initial Fraction creation and initial test project and test

--- FractionMath/Program.cs
+++ FractionMath/Program.cs
@@ -10,6 +10,33 @@ namespace FractionMath
     {
         static void Main(string[] args)
         {
+            Fraction oneFourth = new Fraction("1/4");
+        }
+    }
+
+    public class Fraction
+    {
+
+        public int Numerator { get; private set; }
+        public int Denominator { get; private set; }
+
+        public Fraction(string fractionText)
+        {
+            string[] splitString = fractionText.Split('/');
+            if (splitString.Length < 2)
+                throw new FormatException("Expected string in format xx/yy.");
+
+            for (int i = 0; i < splitString.Length; i++)
+            {
+                int result;
+                if (int.TryParse(splitString[i], out result))
+                    if (i ==0)
+                        Numerator = result;
+                    else
+                        Denominator = result;
+                else
+                    throw new FormatException("Expected string in format xx/yy.");
+            }            
         }
     }
 }
---
+++ FractionMathTest/FractionMathTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FractionMath;
+
+namespace FractionMathTest
+{
+    [TestClass]
+    public class FractionTest
+    {
+        [TestMethod]
+        public void TestFractionCreation()
+        {
+            Fraction fraction = new Fraction("1/4");
+            Assert.AreEqual(1, fraction.Numerator);
+            Assert.AreEqual(4, fraction.Denominator);
+        }
+
+    }
+}
